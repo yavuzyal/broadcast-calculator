52808d61a05bd5bfa4abe4bf6a7c561bc23e3f5d
s21myavuz <myavuz@sabanciuniv.edu>
2022-08-09 20:01:36 +0300

    serverside removed

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 
+var pattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+$("#calcButton").click(function(){
+
+  var subnetIp = $("#ipAddress").val();
+  var subnetMask = $("#subnetMask").val();
+
+  if(!$.trim(subnetIp).match(pattern)){
+    alert('Please enter a valid IP Address!');
+  }
+
+  else if (!$.trim(subnetMask).match(pattern)) {
+    $("#subnetMask").popover('show');
+  }
+
+  else{
+    var numIpOctets = 4;
+
+    const subnetIpArr = ['0','0','0','0'];
+    const subnetMaskArr = ['0','0','0','0'];
+    const calcBroadcastAddrArr = ['0','0','0','0'];
+    var subnetIpStringPos = 0;
+    var subnetMaskStringPos = 0;
+    var calculatedBroadcastAddr = "";
+
+    for(var i = 0; i < numIpOctets; i++){
+      subnetIpArr[i] = parseInt(subnetIp.substr(subnetIpStringPos, subnetIp.indexOf('.', subnetIpStringPos)-subnetIpStringPos));
+      subnetIpStringPos = subnetIp.indexOf('.', subnetIpStringPos) + 1;
+
+      subnetMaskArr[i] = parseInt(subnetMask.substr(subnetMaskStringPos, subnetMask.indexOf('.', subnetMaskStringPos)-subnetMaskStringPos));
+      subnetMaskStringPos = subnetMask.indexOf('.', subnetMaskStringPos) + 1;
+
+      subnetMaskArr[i] = (~subnetMaskArr[i])&(255);
+
+      calcBroadcastAddrArr[i] = subnetIpArr[i] | subnetMaskArr[i];
+
+      if(i < 3)
+      {
+        calculatedBroadcastAddr = calculatedBroadcastAddr + calcBroadcastAddrArr[i].toString() + ".";
+      }
+      else
+      {
+        calculatedBroadcastAddr = calculatedBroadcastAddr + calcBroadcastAddrArr[i].toString();
+      }
+    }
+
+    $("#broadcastAddr").text(calculatedBroadcastAddr);
+  }
+
+});
